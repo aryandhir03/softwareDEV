7a29417a79d3cb320dfc8d6c85e875c4938e81cc
Aryan Dhir <dhiraryan123@gmail.com>
2024-01-24 08:57:48 -0500

    commit1

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+/**
+ * date - 10-jan-2024
+ * @author dhira
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       System.out.println("Hello World!");
+       
+       
+       
+    }
+    
+}
