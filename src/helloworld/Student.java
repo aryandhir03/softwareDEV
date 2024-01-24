43ccd1fcb3959fa94d563fa90e18ef93502e1276
Aryan Dhir <dhiraryan123@gmail.com>
2024-01-24 10:21:45 -0500

    added a part-time std class

--- src/helloworld/Student.java
+++ src/helloworld/Student.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package arraydemo;
+package helloworld;
+
 
 /**
  *This is for fetch and ,merge
